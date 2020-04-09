744d67aeef10afdbbf6116fcd8d6f9b0f81114d9
taroj khan <tarujkhan@yahoo.com>
2020-04-09 19:09:14 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end 
   
   def self.most_popular_show 
-    self.where("rating = ?", highest_rating)
+    self.where("rating = ?", highest_rating)[0]
 end 
 
 def self.lowest_rating
